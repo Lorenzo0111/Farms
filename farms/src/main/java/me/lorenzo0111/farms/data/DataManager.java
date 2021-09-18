19730247b4cfd3601be64c9b082142696bc0abec
Lorenzo <69311874+Lorenzo0111@users.noreply.github.com>
2021-09-18 18:21:17 +0200

    Fixed legacy support

--- farms/src/main/java/me/lorenzo0111/farms/data/DataManager.java
+++ farms/src/main/java/me/lorenzo0111/farms/data/DataManager.java
@@ -31,18 +31,6 @@ public class DataManager {
 
     public void init() {
         for (IFarm farm : farms) {
-            if (farm.getLocation() != null && !farm.getLocation().isWorldLoaded()) {
-                plugin.getLogger().warning("Farm " + farm.getUuid() + " has a location of an unloaded world. This may give some errors. Unloading farm..");
-                plugin.debug("Unloading farm " + farm.getUuid() + ". Reason: LOCATION[" + farm.getLocation().getWorld() + "]");
-                continue;
-            }
-
-            if (farm.getCollector() != null && !farm.getCollector().isWorldLoaded()) {
-                plugin.getLogger().warning("Farm " + farm.getUuid() + " has a collector location of an unloaded world. This may give some errors.");
-                plugin.debug("Unloading farm " + farm.getUuid() + ". Reason: COLLECT_LOCATION[" + farm.getLocation().getWorld() + "]");
-                continue;
-            }
-
             if (((Farm) farm).getTask() != null)
                 continue;
 
